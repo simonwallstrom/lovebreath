b4285b22069be29468816fdd1a5a03e34067715d
Simon Wallström <simon@Simons-MacBook-Air.local>
2020-02-03 23:25:35 +0100

    initial commit

---
+++ .eleventy.js
@@ -0,0 +1,47 @@
+const { DateTime } = require("luxon");
+const htmlmin = require("html-minifier");
+const yaml = require("js-yaml");
+
+module.exports = function (eleventyConfig) {
+    // Folders to copy to build dir (See. 1.1)
+    eleventyConfig.addPassthroughCopy("src/static");
+    
+    if (process.env.ELEVENTY_ENV === 'production') {
+        // Minify HTML (including inlined CSS and JS) 
+        eleventyConfig.addTransform("compressHTML", function (content, outputPath) {
+            if (outputPath.endsWith(".html")) {
+                let minified = htmlmin.minify(content, {
+                    useShortDoctype: true,
+                    removeComments: true,
+                    collapseWhitespace: true,
+                    minifyCSS: true,
+                    minifyJS: true
+                });
+                return minified;
+            }
+            return content;
+        });
+    }
+    
+    // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+    eleventyConfig.addFilter('htmlDateString', (dateObj) => {
+        return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat('yyyy-LL-dd');
+    });
+
+    // Add YAML support for data files
+    eleventyConfig.addDataExtension("yaml", contents => yaml.safeLoad(contents));
+
+    return {
+        dir: {
+            input: "src/",
+            output: "dist",
+            includes: "_includes",
+            layouts: "_layouts"
+        },
+        templateFormats: ["html", "md", "njk"],
+        htmlTemplateEngine: "njk",
+
+        // 1.1 Enable eleventy to pass dirs specified above
+        passthroughFileCopy: true
+    };
+};
---
+++ README.md
@@ -0,0 +1,47 @@
+## Project Overview 
+
+- The project uses [Eleventy](https://11ty.io) as a static site generator
+- Default templating is [Nunjucks](https://mozilla.github.io/nunjucks/) (can be changed if you want)
+- PostCSS set up to handle:
+	- TailwindCSS
+	- Autoprefixer 
+- PurgeCSS to remove unused CSS (set up for TailwindCSS by default) in production
+- HTML minified in production
+- CSS inlined and minified in production
+- Webpack used to bundle scripts
+- Scripts optimised for production
+- Document `<head>` crafted using [htmlhead.dev](https://htmlhead.dev)
+
+---
+
+## Getting Started
+
+### Install dependencies
+
+```
+npm install
+```
+
+### Working locally
+Starts watch tasks to compile when changes detected
+
+```
+npm start
+```
+
+### Creating a production build 
+Minify HTML, compress JS, inline and minify CSS.
+
+``` 
+npm run build
+```
+
+---
+
+## Deployment 
+
+You can host the production output on any web server or service you like and upload it via any method, it'll work. 
+
+If you don't have an existing place to host your site you should have a look at [Netlify](https://www.netlify.com), I can't recommend it enough. To get started you can hit the button below.
+
+[![Deploy to Netlify](https://www.netlify.com/img/deploy/button.svg)](https://app.netlify.com/start/deploy?repository=https://github.com/chrisssycollins/web-starter)
